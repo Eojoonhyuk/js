6dca02b791f57cd7c6baa79eadf4c4173bfa5b63
anftlcb@gmail.com <94384392+Eojoonhyuk@users.noreply.github.com>
2023-03-14 20:44:17 +0900

    영어 끝말잇기

--- js/이진 변환 반복하기.js
+++ js/이진 변환 반복하기.js
@@ -1,11 +1,11 @@
 const solution = (s) => {
-    let answer = [0, 0];
+    const answer = [0, 0];
     while(s.length > 1){
         answer[0]++;
         answer[1] += (s.match(/0/g)||[]).length;
         s = s.replace(/[^(1-9)]/gi,"").length.toString(2);
     }
-    return answer;
+    return console.log(answer);
 }
 
 solution("110010101001");
