07855f3ccb6062da83d668cae494f0671f687971
Eojoonhyuk <anftlcb@gmail.com>
2024-06-28 21:01:30 +0900

    분수의 덧셈

---
+++ 분수의 덧셈.js
@@ -0,0 +1,11 @@
+function solution(numer1, denom1, numer2, denom2) {
+  let numer = numer1 * denom2 + numer2 * denom1;
+  let denom = denom1 * denom2;
+  let gcd = 1;
+  for (let i = 1; i <= numer; i++) {
+    if (numer % i === 0 && denom % i === 0) {
+      gcd = i;
+    }
+  }
+  return [numer / gcd, denom / gcd];
+}
